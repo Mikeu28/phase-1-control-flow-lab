e1f2138e88a4dbc6efba8324de40389543d1f244
Mike Urezzio <mikeurezzio@gmail.com>
2023-03-23 15:23:37 -0400

    control flow lab complete

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  if (rideLength <= 400) {
+    return `This one is on me!`;
+  } 
+  else if (rideLength > 400 && rideLength <= 2000 ) {
+    return `That will be twenty bucks.`;
+  }
+  else if (rideLength > 2000 && rideLength <= 2500) {
+    return `I will gladly take your thirty bucks.`;
+  }
+  else if (rideLength > 2500) {
+    return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === `NYC`) {
+    return `Ok, sounds good.`;
+  } else {
+    return `No go.`;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(expectedTip){
+  if (expectedTip === `generous`) {
+    return `Thank you so much.`;
+  } else if (expectedTip === `not as generous`) {
+    return `Thank you.`;
+  } else {
+    return `Bye.`;
+  }
 }
